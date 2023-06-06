4023e34cc05b49570f1ecf2b449f25022d52eb7f
Lokesh Harsha Vardhan <84661371+LokeshHarshaVardhan@users.noreply.github.com>
2023-06-06 10:37:51 +0530

    import java.util.*; public class simple {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int p,t,r,i;
            p = sc.nextInt();
            t = sc.nextInt();
            r = sc.nextInt();
            i = (p*t*r)/100;
            System.out.println(i);
            sc.close();
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class simple 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int p,t,r,i;
+        
+        p = sc.nextInt();
+        t = sc.nextInt();
+        r = sc.nextInt();
+        i = (p*t*r)/100;
+        System.out.println(i);
+        sc.close();
+    }
+}
